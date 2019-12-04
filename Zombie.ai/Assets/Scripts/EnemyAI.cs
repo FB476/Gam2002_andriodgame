c6789a273ec5130e6a86272b9d3fa8d21abd05da
frederick bajet <FB476@live.mdx.ac.uk>
2019-12-04 18:08:42 +0000

    First Update
    Added zombie AI, menu, player camera, player movement, spawner's

---
+++ Zombie.ai/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,60 @@
+﻿  using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    NavMeshAgent nm;
+    public Transform target;
+
+    public float distanceThreshold = 10f;
+    public enum AIState { idle,chasing};
+
+    public AIState aistate = AIState.idle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nm = GetComponent<NavMeshAgent>();
+        StartCoroutine(Think());
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Think()
+    {
+        while (true)
+        {
+            switch (aistate)
+            {
+                case AIState.idle:
+                    float dist = Vector3.Distance(target.position, transform.position);
+                    if (dist < distanceThreshold)
+                    {
+                        aistate = AIState.chasing; 
+                    }
+                    nm.SetDestination(transform.position);
+                    break;
+                case AIState.chasing:
+                    dist = Vector3.Distance(target.position, transform.position);
+                    if (dist > distanceThreshold)
+                    {
+                        aistate = AIState.idle;
+                    }
+                    nm.SetDestination(target.position);
+                    break;
+                default:
+                    break;
+            }
+            yield return new WaitForSeconds(1f);
+        }
+         
+    }
+}
---
+++ Zombie.ai/Assets/ZombieSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawn : MonoBehaviour
+{
+    public GameObject[] spawnee;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnObject", spawnTime, spawnDelay);
+    }
+
+    public void spawnObject()
+    {
+        int index = Random.Range(0, spawnee.Length);
+        Instantiate(spawnee[index], transform.position, transform.rotation);
+    }
+}
